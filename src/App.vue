0b0066061988a2c23e2338cc357b2e579d26e437
Guillermo Castaño <guillermocastano@Mac-mini-de-Guillermo.local>
2022-09-25 11:37:44 -0500

    Primer Commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app-container">    
+      <aside>
+        <RouterLink to="/">Link</RouterLink>
+      </aside>
+      <main class="wrapper">
+        <RouterView />
+      </main>
+    
+  </div>
+
+  
+</template>
+
+<script setup lang="ts">
+  import { RouterLink, RouterView } from 'vue-router'
+  
+  </script>
+  
+
+<style scoped>
+.app-container {
+  display: flex;
+}
+</style>
---
+++ src/views/PostsView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Posts
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
